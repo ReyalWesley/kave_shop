6c2bcb6e285ab0c9f5b46cf7f56a6e261a01ce76
REYAL Wesley <reyal.wesley@gmail.com>
2023-09-07 14:56:52 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+  // 1. Define route components.
+  // These can be imported from other files
+  const Home = { template: '<div>Home</div>' }
+  const About = { template: '<div>About</div>' }
+
+  // 2. Define some routes
+  const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+  ]
+
+  // 3. Create the router instance and pass the `routes` option
+  const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+  // 5. Create and mount the root instance.
+  const app = createApp({})
+  // Make sure to _use_ the router instance to make the
+  // whole app router-aware.
+  app.use(router)
+
+  app.mount('#app')
